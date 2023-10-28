90c8bcb6d3d67862fc2ec146659bd7552fd813da
nagev <nagevallon.r@gmail.com>
2023-10-28 14:22:05 -0400

    remove test module connections, implement author search and post book route

--- Main/geektext/BookDetails/BookDetailsRepository.py
+++ Main/geektext/BookDetails/BookDetailsRepository.py
@@ -5,14 +5,14 @@ class BookDetailsRepository:
     def __init__(self, db_connection):
         self.db_connection = db_connection
 
-    def get_book_by_id(self, isbn):  # do error checking if the book is not found return error for that
+    def get_book_by_id(self, isbn):  # alter possibly to display author name and not id
         query = "SELECT * FROM books WHERE isbn = %s" #check why this is highlighted?
         result = self.db_connection.execute_query(query, (isbn,))
 
         if not result:
             return {"error": "Book not found"}
 
-        book = []   #NOTE!!! When output the key values are in alphabetical order? Figure this out
+        book = []   #NOTE!!! When output the key values are in alphabetical order? Figure this out or maybe leave it
         for row in result:
             book_format = {
                 "isbn": row[0],
@@ -28,4 +28,26 @@ class BookDetailsRepository:
             book.append(book_format)
         return book
 
-    # add more query methods
+    def get_books_list(self, author_id):
+        query = "SELECT * FROM books WHERE author_id = %s"
+        result = self.db_connection.execute_query(query, (author_id,))
+
+        if not result:
+            return {"error": "Book list not found for author"}
+
+        return result
+
+    def add_book(self, new_book):
+        query = "INSERT INTO books VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        data = (new_book.isbn, new_book.title, new_book.description, new_book.price, new_book.author, new_book.genre,
+                new_book.publisher, new_book.year_pub, new_book.copies_sold)
+        #add query for inserting new book in db
+        #!!!INVESTIGATE DB strcture for books and author: user should be able to input an author while making a book
+        # SHOULD DISPLAY AUTHOR NOT AUTHOR ID??
+
+        result = self.db_connection.execute_query(query, data)
+
+        if not result:
+            return {"error": "Error adding book"}
+
+        return result
--- Main/geektext/BookDetails/Routes.py
+++ Main/geektext/BookDetails/Routes.py
@@ -1,5 +1,7 @@
-from flask import Flask, request, jsonify
 from flask import Blueprint
+from flask import request, jsonify
+
+from Main.geektext.Book import Book
 from Main.geektext.BookDetails.BookDetailsRepository import BookDetailsRepository
 from Main.geektext.resources.DataAccess import db_connection
 
@@ -8,20 +10,43 @@ book_details = Blueprint('book_details', __name__, url_prefix='/book-details')
 
 @book_details.route("/books/<string:isbn>", methods=['GET', 'POST'])
 def get_book(isbn):
-    repository = BookDetailsRepository(db_connection)
-    """""
-    with open('mockdb_books.json', 'r') as f:
-        data = json.load(f)
-        print("Hi I'm working from BookDetails Routes!")
-    """
-    """""
-        for book in data['books']:
-            if book['isbn'] == isbn:
-                return jsonify(book)
-        else:
-            return jsonify({"error": "Book not found"}), 404 # Handle the case when the ISBN is not found
-        """
+    repository = BookDetailsRepository(
+        db_connection)  # repeated calls to this line might make the whole routes file a class and
+    # initilize this line first so reptition is eliminated
+
     if request.method == 'GET':
         return jsonify(repository.get_book_by_id(isbn))
     else:
-        return jsonify({"error": "Book not found"}), 404  # Handle the case when the ISBN is not found
+        return jsonify({"error": "Book not found"}), 404  # possibly delete
+
+
+@book_details.route("/authors/<string:author_id>", methods=['GET'])
+def get_books_list(author_id):
+    repository = BookDetailsRepository(db_connection)
+    return jsonify(repository.get_books_list(author_id))
+
+
+''' Parse the data sent by the client.
+Check if the author exists in the "authors" table based on the author's name (first_name and last_name).
+If the author doesn't exist, create a new author entry in the "authors" table.
+Create a new book entry in the "books" table with the book details, including the author's ID.
+Return a success response to the client.'''
+
+
+@book_details.route("/admin/create-book/", methods=['POST'])
+def create_book():
+    repository = BookDetailsRepository(db_connection)
+    if request.method == 'POST':  # !!!look into another way to receive input instead of request form maybe?
+        isbn = request.form['isbn']
+        title = request.form['title']
+        description = request.form['description']
+        price = request.form['price']
+        author = request.form['author']
+        genre = request.form['genre']
+        publisher = request.form['publisher']
+        year_pub = request.form['year_pub']
+        copies_sold = request.form['copies_sold']
+
+        new_book = Book(isbn, title, description, price, author, genre, publisher, year_pub, copies_sold)
+        # call repository and send the book object to it
+        return (repository.add_book(new_book))
